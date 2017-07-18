97b09d0521080da49c1f969a71f5d4fa01590eb6
julia57 <nuwanda57@yandex.ru>
2017-07-18 20:04:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ db_session = scoped_session(sessionmaker(bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
 
-updater = Updater("TOKEN")
+TOKEN = os.environ.get('TELEGRAM_TOKEN')
+updater = Updater(TOKEN)
 dp = updater.dispatcher
 job_queue = updater.job_queue
